53883270e38e4ed79c96e5f5db2e6ebcc708ef77
dsjclp <dsjclp@gmail.com>
2020-07-24 11:06:09 +0200

    prodV1

--- band/settings/dev.py
+++ band/settings/dev.py
@@ -24,4 +24,24 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "contact@yarig.fr"
 EMAIL_HOST_PASSWORD = "Jclp1999*"
 
+#EMAIL_HOST = "smtp.gmail.com"
+#EMAIL_USE_TLS = True
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = "dsjclp@gmail.com"
+#EMAIL_HOST_PASSWORD = "dslp..."
+
 ADMINS = [('dsjclp', 'dsjclp@gmail.com')]
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_REFERRER_POLICY = 'origin'
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 3600
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
