4d34acbcf317aa1aefd85762d16082c01454dfb3
dsjclp <dsjclp@gmail.com>
2020-07-30 10:37:38 +0200

    banda3

--- band/settings/dev.py
+++ band/settings/dev.py
@@ -5,13 +5,8 @@ import string
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# DJANGO_SECRET_KEY *should* be specified in the environment. If it's not, generate an ephemeral key.
-if 'DJANGO_SECRET_KEY' in os.environ:
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-else:
-    # Use if/else rather than a default value to avoid calculating this if we don't need it
-    print("WARNING: DJANGO_SECRET_KEY not found in os.environ. Generating ephemeral SECRET_KEY.")
-    SECRET_KEY = ''.join([random.SystemRandom().choice(string.printable) for i in range(50)])
+# Generate an ephemeral key.
+SECRET_KEY = ''.join([random.SystemRandom().choice(string.printable) for i in range(50)])
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.yarig.fr']
