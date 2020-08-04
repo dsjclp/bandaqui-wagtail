9f52a0fbe8fdc204ca0d2430e968075a6b28abb2
dsjclp <dsjclp@gmail.com>
2020-08-04 12:47:46 +0200

    prod17

--- events/models.py
+++ events/models.py
@@ -226,7 +226,8 @@ class EventCalendar(RoutablePageMixin, Page):
 
     def get_context(self, request):
         context = super().get_context(request)
-        context['nextevent'] = EventCalPage.objects.filter(start_dt__gte=timezone.now()).first()
+        next_event = EventCalPage.objects.filter(start_dt__gte=timezone.now(), categories__slug='public').order_by('start_dt').first()
+        context['nextevent'] = next_event
         return context
 
 class EventCalPage(RoutablePageMixin, Page):
