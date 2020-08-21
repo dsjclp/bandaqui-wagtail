1782b33dd26fc13ca9a2fed09af56cdfbcd79b84
dsjclp <dsjclp@gmail.com>
2020-08-21 10:31:29 +0200

    prod26

--- home/models.py
+++ home/models.py
@@ -21,14 +21,6 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     ) 
-
-    carousellogo = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    ) 
     
     carouseltext1 = RichTextField(blank=True, null=True)
 
@@ -79,7 +71,7 @@ class HomePage(Page):
     # Editor panels configuration
 
     content_panels = Page.content_panels + [
-        ImageChooserPanel('carousellogo'),
+        #ImageChooserPanel('carousellogo'),
         FieldPanel('carouseltext1', classname="full"),
         ImageChooserPanel('carouselimage1'),
         FieldPanel('carouseltext2', classname="full"),
